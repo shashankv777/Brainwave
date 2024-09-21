14db4020418170aa6947412de1708dca2421a640
shashank <shashankv9353@gmail.com>
2024-09-21 13:58:43 +0530

    changes done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ import CompanyLogo from "./CompanyLogo"
 const Hero = () => {
     const parallaxRef = useRef(null)
   return (
-   <Section className="pt-[12rem] -mt-[5.25rem]" crosses crossesOffset="lg:translate-y-[5.25rem]" customPaddings id="hero">
+   <Section className="pt-[12rem] -mt-[5.25rem]" crosses crossesOffset="lg:translate-y-[5.25rem]" customPaddings id="features">
         <div className="container relative">
             <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
                 <h1 className="h1 mb-6">
