d1785cf2e8e67f92ef13f8e56108fb301569d749
shashank <shashankv9353@gmail.com>
2024-09-21 13:43:24 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header/>
         <Hero/>
-        <Benefits/>
         <Collaboration/>
         <Services/>
         <Pricing/>
